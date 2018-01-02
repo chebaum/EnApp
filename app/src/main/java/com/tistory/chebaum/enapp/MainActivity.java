5471a524e8b634c21527c3dcff3184295733d648
yscho1219 <lala1109>
2018-01-02 18:20:21 +0900

    111

--- app/src/main/java/com/tistory/chebaum/enapp/MainActivity.java
+++ app/src/main/java/com/tistory/chebaum/enapp/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-// 주석추가
+// 주석추가111
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
