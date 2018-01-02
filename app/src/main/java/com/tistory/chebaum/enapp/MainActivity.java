68649c233e03abeb169610317c6959c5a7ed4c7e
chebaum <chebaum@naver.com>
2018-01-02 18:18:02 +0900

    테스트 커밋

--- app/src/main/java/com/tistory/chebaum/enapp/MainActivity.java
+++ app/src/main/java/com/tistory/chebaum/enapp/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+// 주석추가
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
